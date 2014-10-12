824764d7a3293268401fabce138ced176f648a3b
wusuopu <admin@longchangjin.cn>
2014-10-12 22:16:34 +0800

    添加测试用例

--- src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
+++ src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
@@ -4,6 +4,20 @@ namespace Blogger\BlogBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * PageControllerTest
+ */
 class PageControllerTest extends WebTestCase
 {
+    /**
+     * test index page.
+     */
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertTrue($crawler->filter('title:contains("symblog")')->count() > 0);
+    }
 }
